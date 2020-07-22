d614ea51a0328495a6c001a64200298e32885325
Travis Vachon <travis.vachon@gmail.com>
2020-07-22 09:50:39 -0700

    remove usePostContainer
    this is a domain-specific thing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export { useContainer } from './hooks/useContainer'
 export { useEnsured } from './hooks/useEnsured'
-export { usePostContainer } from './hooks/usePostContainer'
 export { useProfile } from './hooks/useProfile'
 export { useThing } from './hooks/useThing'
 export { useWebId } from './hooks/useWebId'
