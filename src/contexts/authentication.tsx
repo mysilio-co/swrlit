3934a79073dca8dc3a945babd765434f01a5b3dd
Travis Vachon <travis.vachon@gmail.com>
2020-08-23 16:40:31 -0700

    switch to @inrupt/solid-client-authn-browser
    move off outdated solid-auth-client

--- src/contexts/authentication.tsx
+++ src/contexts/authentication.tsx
@@ -1,5 +1,10 @@
-import React, { createContext, useContext } from 'react'
-import auth from 'solid-auth-client'
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import {
+  Session,
+  SessionManager,
+  getClientAuthenticationWithDependencies
+} from '@inrupt/solid-client-authn-browser'
+
 import { getSolidDataset } from "@itme/solid-client"
 
 export type fetcherFn<Data> = (...args: any) => Data | Promise<Data>
@@ -12,31 +17,75 @@ type Authentication = {
   logout: () => Promise<void>
 }
 
+const defaultFetch = async (url: string, options: any) => {
+  console.error("No default fetch implementation - have you added an AuthenticationProvider at the top level of your app?")
+}
+
+const defaultLogin = async (options: any) => {
+  console.error("No default login implementation - have you added an AuthenticationProvider at the top level of your app?")
+}
+
+const defaultPopupLogin = async () => {
+  console.error("No default popupLogin implementation - have you added an AuthenticationProvider at the top level of your app?")
+}
+
+const defaultLogout = async () => {
+  console.error("No default logout implementation - have you added an AuthenticationProvider at the top level of your app?")
+}
+
 const AuthenticationContext = createContext<Authentication>({
   session: null,
-  login: async (options) => {
-    console.error("No default login implementation - have you added an AuthenticationProvider at the top level of your app?")
-  },
-  popupLogin: async () => {
-    console.error("No default popupLogin implementation - have you added an AuthenticationProvider at the top level of your app?")
-  },
-  fetch: async (url, options) => {
-    console.error("No default fetch implementation - have you added an AuthenticationProvider at the top level of your app?")
-  },
-  logout: async () => {
-    console.error("No default logout implementation - have you added an AuthenticationProvider at the top level of your app?")
-  }
+  login: defaultLogin,
+  popupLogin: defaultPopupLogin,
+  fetch: defaultFetch,
+  logout: defaultLogout
 });
 
 const { Provider, Consumer } = AuthenticationContext
 
+class BrowserStorage {
+  async get(key: string) {
+    return window.localStorage.getItem(key) || undefined;
+  }
+  async set(key: string, value: any) {
+    window.localStorage.setItem(key, value);
+  }
+  async delete(key: string) {
+    window.localStorage.removeItem(key);
+  }
+}
+
 export const AuthenticationProvider = (props: any) => {
+  const [sessionManager, setSessionManager] = useState<SessionManager>(new SessionManager({
+    // this isn't great, but is noted as a problem in the library here:
+    //https://github.com/inrupt/solid-client-authn-js/blob/70cd413405667de4abd0e3fde922e7205a6e5e53/src/login/oidc/ClientRegistrar.ts#L77
+    // and in any case is exactly what solid-auth-client was doing
+    secureStorage: new BrowserStorage()
+  }));
+  const [session, setSession] = useState<Session | undefined>()
+  useEffect(() => {
+    async function fetchSession() {
+      setSession(await sessionManager.getSession("default"))
+    }
+    fetchSession()
+  }, [])
+
   const value = ({
-    session: auth,
-    fetch: auth.fetch,
-    login: auth.login,
-    logout: auth.logout,
-    popupLogin: auth.popupLogin
+    session,
+    fetch: session ? session.fetch : defaultFetch,
+
+    login: session ? async (args: any) => {
+      await session.login(args)
+      setSession(await sessionManager.getSession("default"))
+    } : defaultLogin,
+    popupLogin: session ? async (args: any) => {
+      await session.login({ popUp: true, ...args })
+      setSession(await sessionManager.getSession("default"))
+    } : defaultPopupLogin,
+    logout: session ? async () => {
+      await session.logout()
+      setSession(await sessionManager.getSession("default"))
+    } : defaultLogout
   })
   return (
     <Provider value={value}  {...props} />
