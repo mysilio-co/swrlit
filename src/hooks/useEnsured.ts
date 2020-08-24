f590a2eed0eb695736269294676e9324e3aa0e23
Travis Vachon <travis.vachon@gmail.com>
2020-08-23 19:55:37 -0700

    update all the http helpers to take a session
    globals are bad

--- src/hooks/useEnsured.ts
+++ src/hooks/useEnsured.ts
@@ -1,23 +1,25 @@
 import { useState, useEffect } from "react"
 
 import { resourceExists, createDocument, deleteFile } from '../lib/http'
+import { useAuthentication } from '../contexts/authentication'
 
 export function useEnsured(url: string | undefined | null) {
-  const [ensuredUrl, setEnsuredUrl] = useState<string | undefined | null>()
-  useEffect(() => {
-    async function ensureUrl(u: string) {
-      if (await resourceExists(u)) {
-        setEnsuredUrl(u)
-      } else {
-        await createDocument(`${u}.dummy`)
-        await deleteFile(`${u}.dummy`)
-        setEnsuredUrl(u)
-      }
+    const [ensuredUrl, setEnsuredUrl] = useState<string | undefined | null>()
+    const { session } = useAuthentication()
+    useEffect(() => {
+        async function ensureUrl(u: string) {
+            if (await resourceExists(session, u)) {
+                setEnsuredUrl(u)
+            } else {
+                await createDocument(session, `${u}.dummy`)
+                await deleteFile(session, `${u}.dummy`)
+                setEnsuredUrl(u)
+            }
 
-    }
-    if (url) {
-      ensureUrl(url)
-    }
-  }, [url])
-  return ensuredUrl
+        }
+        if (url && session) {
+            ensureUrl(url)
+        }
+    }, [url, session])
+    return ensuredUrl
 }
--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,11 +1,11 @@
-import auth from 'solid-auth-client';
+import { Session } from '@inrupt/solid-client-authn-browser'
 
-export const resourceExists = async (resourcePath: string) => {
+export const resourceExists = async (auth: Session, resourcePath: string) => {
     const result = await auth.fetch(resourcePath, { method: 'HEAD' });
     return result.status === 403 || result.status === 200;
 };
 
-export const createDoc = async (documentUri: string, options: any) => {
+export const createDoc = async (auth: Session, documentUri: string, options: any) => {
     try {
         return await auth.fetch(documentUri, options);
     } catch (e) {
@@ -13,7 +13,7 @@ export const createDoc = async (documentUri: string, options: any) => {
     }
 };
 
-export const createDocument = async (documentUri: string, body = '') => {
+export const createDocument = async (auth: Session, documentUri: string, body = '') => {
     try {
         const options = {
             method: 'PUT',
@@ -22,13 +22,13 @@ export const createDocument = async (documentUri: string, body = '') => {
             },
             body
         };
-        return await createDoc(documentUri, options);
+        return await createDoc(auth, documentUri, options);
     } catch (e) {
         throw e;
     }
 };
 
-export const deleteFile = async (url: string) => {
+export const deleteFile = async (auth: Session, url: string) => {
     try {
         return await auth.fetch(url, { method: 'DELETE' });
     } catch (e) {
