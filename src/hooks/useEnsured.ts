6316d3f4f71ceb90928da018fc7315db113ce317
Travis Vachon <travis.vachon@gmail.com>
2021-01-14 09:50:56 -0800

    add ability to cache useEnsured so we don't even make HEAD requests
    they were pretty overwhelming

--- src/hooks/useEnsured.ts
+++ src/hooks/useEnsured.ts
@@ -3,17 +3,28 @@ import { useState, useEffect } from "react"
 import { resourceExists, createDocument, deleteFile } from '../lib/http'
 import { useAuthentication } from '../contexts/authentication'
 
-export function useEnsured(url: string | undefined | null) {
+type UseEnsuredOptions = {
+  useCached: boolean
+}
+
+const ensuredCache: any = {}
+
+export function useEnsured(url: string | undefined | null, { useCached = true }: UseEnsuredOptions = {useCached: true}) {
   const [ensuredUrl, setEnsuredUrl] = useState<string | undefined | null>()
   const { fetch } = useAuthentication()
   useEffect(() => {
     async function ensureUrl(u: string) {
-      if (await resourceExists(fetch, u)) {
+      if (useCached && ensuredCache[u]){
         setEnsuredUrl(u)
       } else {
-        await createDocument(fetch, `${u}.dummy`)
-        await deleteFile(fetch, `${u}.dummy`)
-        setEnsuredUrl(u)
+        ensuredCache[u] = true
+        if (await resourceExists(fetch, u)) {
+          setEnsuredUrl(u)
+        } else {
+          await createDocument(fetch, `${u}.dummy`)
+          await deleteFile(fetch, `${u}.dummy`)
+          setEnsuredUrl(u)
+        }
       }
     }
     if (url && fetch) {
