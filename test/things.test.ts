dd11d94e4bc1eb30428d30cf410df0377dcc9945
Travis Vachon <travis.vachon@gmail.com>
2021-01-28 15:11:42 -0800

    set de-duping interval to 0
    thanks, https://medium.com/frontend-digest/using-testing-libary-with-useswr-f595919de2fd

--- test/things.test.ts
+++ test/things.test.ts
@@ -1,5 +1,6 @@
 import 'whatwg-fetch'
-import { renderHook, act } from '@testing-library/react-hooks'
+import { act } from '@testing-library/react-hooks'
+import { renderHook } from './utils'
 import { givenMolid } from 'molid/lib/molid-jest';
 
 import {
