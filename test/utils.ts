aac695dbeb425609cb9061f11cddc8c0657e52c8
Travis Vachon <travis.vachon@gmail.com>
2021-10-19 19:49:18 -0700

    refactor wrapper into its own tsx module
    I did this in an unsuccessful attempt to fix a weird compile error. It didn't fix it but feels right anyway.

---
+++ test/utils.ts
@@ -0,0 +1,6 @@
+import { renderHook as upstreamRenderHook, RenderHookOptions, RenderHookResult } from '@testing-library/react-hooks';
+import wrapper from "./wrapper";
+
+export function renderHook(callback: (props?: any) => any, options?: RenderHookOptions<any>): RenderHookResult<any, any> {
+  return upstreamRenderHook(callback, { wrapper, ...options })
+}
---
+++ test/wrapper.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SWRConfig } from "swr";
+
+export default function wrapper({ children }: { children?: React.ReactNode }) {
+  return (
+    <SWRConfig value={{ dedupingInterval: 0, provider: () => new Map() }}>
+      {children}
+    </SWRConfig>
+  );
+};
+
