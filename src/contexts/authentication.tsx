f234eecc9dc85d4c60cff93c56014abef71ed406
Travis Vachon <travis.vachon@gmail.com>
2021-10-22 14:59:32 -0700

    add support for onSessionRestore
    clients need to manually restore the previous URL using onSessionRestore, this lets them do that like:
    
    ```<AuthenticationProvider onSessionRestore={url => router.push(url)}>
    ...
    </AuthenticationProvider>
    ```

--- src/contexts/authentication.tsx
+++ src/contexts/authentication.tsx
@@ -7,9 +7,12 @@ import {
   //  onSessionRestore,
   ISessionInfo
 } from "@inrupt/solid-client-authn-browser";
+import {
+  onSessionRestore
+} from "@inrupt/solid-client-authn-browser";
 
 type Authentication = {
-  info: ISessionInfo,
+  info: ISessionInfo | undefined,
   fetch: Session['fetch'],
   login: Session['login'],
   logout: Session['logout']
@@ -23,28 +26,34 @@ const defaultContext = {
 }
 
 export const AuthenticationContext = createContext<Authentication>(defaultContext);
-
-export function AuthenticationProvider(props: any) {
-  const [info, setInfo] = useState<ISessionInfo>()
-  useEffect(function () {
-    handleIncomingRedirect({
-      restorePreviousSession: true
-    }).then((newInfo) => {
-      // don't set this to undefined values - TODO: is this the right thing to do?
-      if (newInfo) {
-        setInfo(newInfo)
-      }
-    })
-  }, [])
-
-  const swrlitLogout = useCallback(async function swrlitLogout() {
-    await solidLogout()
-    setInfo(getDefaultSession().info)
-  }, [])
-
-  const value = useMemo(() => ({ info, fetch, login, logout: swrlitLogout }), [info])
-  return <AuthenticationContext.Provider value={value} {...props} />
+interface AuthenticationContextProps {
+  onSessionRestore: (url: string) => any
 }
+export const AuthenticationProvider: React.FC<AuthenticationContextProps> =
+  ({ onSessionRestore: sessionRestoreCallback, ...props }) => {
+    const [info, setInfo] = useState<ISessionInfo>()
+
+    useEffect(function () {
+      sessionRestoreCallback && onSessionRestore(sessionRestoreCallback)
+
+      handleIncomingRedirect({
+        restorePreviousSession: true
+      }).then((newInfo) => {
+        // don't set this to undefined values - TODO: is this the right thing to do?
+        if (newInfo) {
+          setInfo(newInfo)
+        }
+      })
+    }, [])
+
+    const swrlitLogout = useCallback(async function swrlitLogout() {
+      await solidLogout()
+      setInfo(getDefaultSession().info)
+    }, [])
+
+    const value = useMemo(() => ({ info, fetch, login, logout: swrlitLogout }), [info])
+    return <AuthenticationContext.Provider value={value} {...props} />
+  }
 
 export const useAuthentication = () => useContext(AuthenticationContext)
 
