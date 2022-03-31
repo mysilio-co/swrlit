4ea70db354b3e4c5b1fa25356e559a8b25509d6a
Travis Vachon <travis.vachon@gmail.com>
2022-03-31 14:05:42 -0700

    add instructions for making build really really big

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-import "@inrupt/solid-client"
+
+// to see big builds, uncomment lines below and run:
+// npm run build && npm run size
+
+// uncomment to add 100k to commonjs BUT NOT esm build
+// import "@inrupt/solid-client"
+
+// uncomment to add 100k to both commonjs and esm builds
+// import "@inrupt/solid-client-authn-browser"
+
+// uncomment both to add 200k to commonjs build and 100k to esm build - this is the normal state
+// import "@inrupt/solid-client"
+// import "@inrupt/solid-client-authn-browser"
+
 //export * from './hooks/useEnsured'
 //export * from './hooks/things'
 //export * from './hooks/access'
