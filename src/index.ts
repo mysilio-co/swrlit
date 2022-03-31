ec686f33e15e9fe6a1a4d37d10560b57349a666a
Travis Vachon <travis.vachon@gmail.com>
2022-03-31 14:00:37 -0700

    strip out everything but solid-client

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-export * from './hooks/useEnsured'
-export * from './hooks/things'
-export * from './hooks/access'
-export * from './contexts/authentication'
-export * from './contexts/pubsub'
+import "@inrupt/solid-client"
+//export * from './hooks/useEnsured'
+//export * from './hooks/things'
+//export * from './hooks/access'
+//export * from './contexts/authentication'
+//export * from './contexts/pubsub'
